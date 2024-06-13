76c07ea9a73a8973e8d481f5d1cb4b2f1be1ec7a
RussellOsias <159703036+RussellOsias@users.noreply.github.com>
2024-06-14 00:44:58 +0800

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<footer class="main-footer">
+    <!-- Copyright notice -->
+    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+    All rights reserved.
+    <!-- Version information -->
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 3.2.0
+    </div>
+    <!-- Terms of Service and Privacy Policy -->
+    <div>
+        <a href="terms_of_service.php">Terms of Service</a> | <a href="privacy_policy.php">Privacy Policy</a>
+    </div>
+</footer>
+</div>
